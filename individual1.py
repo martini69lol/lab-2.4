037aae8bf5b2dabe47939a270e89f5354a054237
Marat <125448969+martini69lol@users.noreply.github.com>
2023-05-11 19:41:02 +0300

    Update individual1.py

--- individual1.py
+++ individual1.py
@@ -1,11 +1,17 @@
-def ten_element():
-    num_list = input('Введите 10 чисел через зяпятую: ').split(',')
-    num_list = list(map(int, num_list))
-    result = 0
-    for i in num_list:
-        if 3 < i < 8:
-            result += i
-    print(result)
-    
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
-ten_element()
+import sys
+
+if __name__ == '__main__':
+    def ten_element():
+        num_list = input('Введите 10 чисел через зяпятую: ').split(',')
+        num_list = list(map(int, num_list))
+        result = 0
+        for i in num_list:
+            if 3 < i < 8:
+                result += i
+        print(result)
+
+
+    ten_element()
